3678c288e8fa1abee0c3f2414fdc1216c1717223
scvkurt03 <kurtgray@gmail.com>
2021-12-01 21:26:02 -0800

    rough draft

--- script.js
+++ script.js
@@ -1,65 +1,113 @@
 const numButtons = Array.from(document.getElementsByClassName("num"))
 const operatorButtons = Array.from(document.getElementsByClassName("operator"))
-const numText = numButtons.map(button => button.textContent)
-const operatorText = operatorButtons.map(button => button.textContent)
-const currentInputDisplay = document.querySelector('.ci')
+const equalsButton = document.querySelector('.equal')
+const clearButton = document.querySelector('.clear')
+const deleteButton = document.querySelector('.delete')
+const currentInputDiv = document.querySelector('.ci')
+const previousInputDiv = document.querySelector('.pi')
+const posNeg = document.querySelector('.posNeg')
 
-let displayValue //defined later 
-let prevDispVal
 
-//operator button listener 
-operatorButtons.forEach(button => {
-    button.addEventListener('click', e => {
-    // store the current display value in prevDispVal variable?
-    prevDispVal = displayValue // LEFT OFF HERE - NEED TO DISPLAY THIS VALUE IN PREV (GET REFERENCE)
-    //THEN CLEAR DISPLAYVALUE
-    console.log(prevDispVal)
-    displayValue = currentInputDisplay.append(e.target.textContent)
+let operation = undefined
+let currDispVal = 0
+let prevDispVal = ''
+let result = ''
+
+const workButtons = () => {
+    numButtons.forEach(button => {
+        button.addEventListener('click', e => { 
+            if(button.textContent === "." && currDispVal.includes('.')) return
+            if(currDispVal === '0' || currDispVal === 0){
+                currDispVal = ''
+            } else {}   
+            currDispVal += button.textContent
+            updateDisplay()                 
+        })
     })
-})
-
-//NOTE: EQUALS BUTTON CALLS OPERATE FUNCTION
-
-//number button listener
-numButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        currentInputDisplay.append(e.target.textContent)        
-        displayValue = currentInputDisplay.textContent
-        console.log(displayValue)
+    clearButton.addEventListener('click', () => {
+        (operation) = ''
+            currDispVal = '0' 
+            prevDispVal = ''
+            num1 = ''
+            num2 = ''
+            currentInputDiv.textContent = ''
+            previousInputDiv.textContent =  ''
     })
-    
-})
-
-//keyboard listener
-window.addEventListener('keyup', e => {
-    if(e.keyCode >= 48 && e.keyCode <= 57){
-        let numKey = e.keyCode
-        currentInputDisplay.append(Number(e.key))
-        displayValue = currentInputDisplay.textContent
-        console.log(displayValue)
-
-    //} else if(//operators){
-        //do something
-    }
-    //console.log(e)
-})
-
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-
-const operate = (o, num1, num2) => {
-    num1 = Number(num1)
-    num2 = Number(num2)
-    if(o = '*'){
-        return multiply(num1, num2)
-    } else if(o = '+'){
-        return add(num1, num2)
-    } else if(o = '-'){
-        return subtract(num1, num2)
-    } else if(o = '÷'){
-        if (num2 === 0) return null
-        return divide(num1, num2)
-    }
+    deleteButton.addEventListener('click', () => {
+        let str = currentInputDiv.textContent
+        if(str != null && str.length > 0){
+            str = str.substring(0, str.length - 1, )
+            currentInputDiv.textContent = str
+        }    
+    })
+    operatorButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if(currDispVal === '') return
+            operation = e.target.textContent
+            calcResult()
+            updateDisplay()
+        }) 
+    })
+    equalsButton.addEventListener('click', () => {
+        operate()
+        updateDisplay()
+    })
+    posNeg.addEventListener('click', () => {
+        if(currDispVal === 0){
+            return
+        } else if(currDispVal < 0){
+            currentInputDiv.textContent.substring(1)
+            currDispVal = currentInputDiv.textContent
+        } else if(currDispVal > 0){
+            currentInputDiv.textContent = `-${currDispVal}`
+            currDispVal = currentInputDiv.textContent
+        }
+    })
 }
+workButtons()
+ 
+const operate = () => {
+    let num1 = parseFloat(prevDispVal)
+    console.log(num1)
+    let num2 = parseFloat(currDispVal)
+    console.log(operation)
+    operation === '*' ? result = num1 * num2
+    : operation === '+' ? result = num1 + num2
+    : operation === '-' ? result = num1 - num2
+    : operation === '÷' && num2 === 0 ? result = "You can't do that, dummy."
+    : operation === '÷' ? result = num1 / num2
+    : ''
+    console.log(result)
+    currDispVal = result
+    prevDispVal = ''
+    operation = undefined
+}
+
+const calcResult = () => {
+    console.log('calcR')
+    if(currDispVal === '') return 
+    if(prevDispVal !== '') {
+        console.log('made it')
+        operate()
+    }
+    prevDispVal = `${currDispVal} ${operation}`
+    currDispVal = ''
+}
+
+const updateDisplay = () => {
+    currentInputDiv.textContent = currDispVal
+    previousInputDiv.textContent = prevDispVal
+}
+
+
+// //keyboard listener
+// window.addEventListener('keyup', e => {
+//     if(e.keyCode >= 48 && e.keyCode <= 57){
+//         let numKey = e.keyCode
+//         currentInputDiv.append(Number(e.key))
+//         currDispVal = currentInputDiv.textContent
+//     //} else if(//operators){
+//         //do something
+//     }
+//     //console.log(e)
+// })
